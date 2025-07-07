00c14e2eeca547319fd5bb90523c1f4c4adfbc18
Erhahon Imafidon <mafiosco4@gmail.com>
2025-07-07 09:50:34 +0100

    feat: integrate apiSlice into the store for enhanced API state management and added it's middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from '../features/todos/todosSlice';
+import { apiSlice } from '../features/api/apiSlice.ts';
 
 export const store = configureStore({
     reducer: {
         todos: todoReducer,
+        [apiSlice.reducerPath]: apiSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export type AppStore = typeof store;
